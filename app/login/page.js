43b43c5494783a0f7f9509345d2a7cfb831964de
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-07-31 17:35:52 +0200

    initiialized project structure

--- app/login/page.js
+++
@@ -1,9 +0,0 @@
-import {Login} from '@/components/login/Login'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-24 w-full">
-     <Login/>
-    </main>
-  );
-}
