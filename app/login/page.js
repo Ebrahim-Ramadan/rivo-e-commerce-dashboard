15cce70ede081fe4da04b1d263eb3de78dbb8a81
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-07-31 18:01:23 +0200

    added login route

---
+++ app/login/page.js
@@ -0,0 +1,9 @@
+import { Login } from "@/components/login/Login";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between  md:-24 w-full">
+      <Login/>
+    </main>
+  );
+}
