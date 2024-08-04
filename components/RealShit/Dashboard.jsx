a0e31034dd63df424bc33220c6527c41834dbaad
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-08-04 13:16:40 +0200

    finalized the orders re-fetching logic after deletion

--- components/RealShit/Dashboard.jsx
+++ components/RealShit/Dashboard.jsx
@@ -1,28 +1,26 @@
-'use client'
+'use client';
 import React, { useEffect, useState } from 'react';
 import mobile from '@/public/assets/mobile.svg';
 import visa from '@/public/assets/visa.svg';
 import Image from "next/image";
 import { deleteOrderById, getProductDetails } from '@/lib/utils';
 import LoadingDots from '../LoadingDots';
-import { useRouter } from 'next/navigation';
+import { useRouter, redirect } from 'next/navigation';
 import { toast } from 'react-hot-toast';
-export const Dashboard = ({ orders, loading, setloading }) => {
+
+export const Dashboard = ({ orders, refetchOrders }) => {
   const [productDetails, setProductDetails] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const router = useRouter();
+  
   useEffect(() => {
     const fetchProductDetails = async (orders) => {
-      // Flatten all items from each order into a single array of promises
       const promises = orders.flatMap(order => order.items.map(item => getProductDetails(item.id)));
 
       try {
-        // Resolve all promises concurrently
         const results = await Promise.all(promises);
-
-        // Map results to the corresponding product IDs
         const details = results.reduce((acc, productDetail, index) => {
-          // Find the order and item for the current index
           let currentIndex = index;
           for (const order of orders) {
             if (currentIndex < order.items.length) {
@@ -37,7 +35,6 @@ export const Dashboard = ({ orders, loading, setloading }) => {
         setProductDetails(details);
       } catch (error) {
         console.error('Error fetching product details:', error);
-        // Handle error accordingly
       }
     };
 
@@ -46,102 +43,105 @@ export const Dashboard = ({ orders, loading, setloading }) => {
     }
   }, [orders]);
 
-  if(loading){
-<div className="flex items-center justify-center w-full h-screen">
-<LoadingDots/>
-
-</div>
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center w-full h-screen">
+        <LoadingDots />
+      </div>
+    );
   }
+
   return (
-    <div className=' flex flex-col gap-4'>
+    <div className='flex flex-col gap-4'>
       {orders.map((order) => {
-          const orderId = Object.keys(order)[0];
-          const orderData = order[orderId];
+        const orderId = Object.keys(order)[0];
+        const orderData = order[orderId];
 
-        return (  <div key={order.id} className={`flex flex-col md:flex-row gap-6 border border-white/20 p-4 rounded-lg`}>
-        <div className="rounded-lg shadow-md flex-grow">
-          <h2 className="text-xl font-semibold mb-1">Order Doc ID {order.id}</h2>
-          <p className="mb-6">Placed on {formatCreatedAt(order.createdAt)}</p>
-          
-          <h3 className="text-lg font-semibold mb-2">Delivery Details</h3>
-          <p className="text-green-600 mb-4">{order.status} · Confirmed</p>
-          
-          {order.items?.map((item, index) => (
-            <a key={index} className="bg-white/10 hover:bg-white/20 flex items-center p-4 mb-2 rounded-lg justify-between" target='_blank' href={`https://e-commerce-myass.vercel.app/frame/${item.id}?type=${item.type}&size=${item.size}&color=${item.color}`}>
-              <div className="flex flex-row gap-2 items-center">
-                <span className="leading-tight relative">
-                  {productDetails[item.id]?.images[0] ? (
-                    <Image
-                      className="rounded-lg"
-                      src={productDetails[item.id]?.images[0]}
-                      alt={productDetails[item.id]?.id}
-                      width={100}
-                      height={150}
-                    />
-                  ) : (
-                    <div className="h-20 flex flex-col items-center justify-center">loading...</div>
-                  )}
-                  <div className="absolute -top-2 -left-2 w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center font-medium text-white">
-                    <div className="">x{item.quantity}</div>
+        return (
+          <div key={order.id} className={`flex flex-col md:flex-row gap-6 border border-white/20 p-4 rounded-lg`}>
+            <div className="rounded-lg shadow-md flex-grow">
+              <h2 className="text-xl font-semibold mb-1">Order Doc ID {order.id}</h2>
+              <p className="mb-6">Placed on {formatCreatedAt(order.createdAt)}</p>
+
+              <h3 className="text-lg font-semibold mb-2">Delivery Details</h3>
+              <p className="text-green-600 mb-4">{order.status} · Confirmed</p>
+
+              {order.items?.map((item, index) => (
+                <a key={index} className="bg-white/10 hover:bg-white/20 flex items-center p-4 mb-2 rounded-lg justify-between" target='_blank' href={`https://e-commerce-myass.vercel.app/frame/${item.id}?type=${item.type}&size=${item.size}&color=${item.color}`}>
+                  <div className="flex flex-row gap-2 items-center">
+                    <span className="leading-tight relative">
+                      {productDetails[item.id]?.images[0] ? (
+                        <Image
+                          className="rounded-lg"
+                          src={productDetails[item.id]?.images[0]}
+                          alt={productDetails[item.id]?.id}
+                          width={100}
+                          height={150}
+                        />
+                      ) : (
+                        <div className="h-20 flex flex-col items-center justify-center">loading...</div>
+                      )}
+                      <div className="absolute -top-2 -left-2 w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center font-medium text-white">
+                        <div className="">x{item.quantity}</div>
+                      </div>
+                    </span>
+
+                    <div className="text-xs">
+                      <p className="font-medium text-base">{productDetails[item.id]?.name}</p>
+                      <p>{item.type}</p>
+                      <p>{item.color} - {item.size}</p>
+                    </div>
                   </div>
-                </span>
+                </a>
+              ))}
+            </div>
 
-                <div className="text-xs">
-                  <p className="font-medium text-base">{productDetails[item.id]?.name}</p>
-                  <p>{item.type}</p>
-                  <p>{item.color} - {item.size}</p>
+            <div className="rounded-lg shadow-md w-full md:w-80">
+              <h3 className="text-lg font-semibold mb-4">Order invoice</h3>
+              <div className="space-y-2 mb-4">
+                <div className="flex justify-between font-bold">
+                  <span>Order total</span>
+                  <span>EGP {calculateTotalPrice(order.items).toFixed(2)}</span>
                 </div>
               </div>
-            </a>
-          ))}
-        </div>
-        
-        <div className="rounded-lg shadow-md w-full md:w-80">
-          <h3 className="text-lg font-semibold mb-4">Order invoice</h3>
-          <div className="space-y-2 mb-4">
-            <div className="flex justify-between font-bold">
-              <span>Order total</span>
-              <span>EGP {calculateTotalPrice(order.items).toFixed(2)}</span>
+
+              <div className="flex justify-end items-center mb-4">
+                <div className="flex items-center gap-2">
+                  {orderData.source_data?.type === 'wallet' ? (
+                    <Image src={mobile} alt="card logo" className="w-6 h-6" width={40} height={40} />
+                  ) : (
+                    <Image src={visa} alt="card logo" className="w-6 h-6" width={40} height={40} />
+                  )}
+                  <span>Ending in {orderData.source_data?.phone_number?.slice(-4)}</span>
+                </div>
+              </div>
+
+              <h3 className="text-lg font-semibold mb-2">Delivery address (Home)</h3>
+              <p className="mb-1 text-sm ">{orderData.shipping_data?.first_name} {orderData.shipping_data?.email}</p>
+              <p className="mb-1">{orderData.shipping_data?.street}, {orderData.shipping_data?.city}, {orderData.shipping_data?.country}</p>
+              <p className="flex items-center">
+                {orderData.shipping_data?.phone_number}
+                <span className="ml-2 text-green-600 text-sm">Verified</span>
+              </p>
+            </div>
+            <div className="flex justify-end md:block">
+              <button className="bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded-xl"
+                onClick={async () => {
+                  setLoading(true);
+                  const deletedID = await deleteOrderById(order.id);
+                  if (deletedID) {
+                    toast.success('Order deleted successfully');
+                    setLoading(false);
+                    await refetchOrders();
+                  } else {
+                    toast.error('Something went wrong');
+                    setLoading(false);  // Ensure to set loading to false in case of error
+                  }
+                }}
+              >Delete Order</button>
             </div>
           </div>
-          
-          <div className="flex justify-end items-center mb-4">
-            <div className="flex items-center gap-2">
-              {orderData.source_data?.type === 'wallet' ? (
-                <Image src={mobile} alt="card logo" className="w-6 h-6" width={40} height={40} />
-              ) : (
-                <Image src={visa} alt="card logo" className="w-6 h-6" width={40} height={40} />
-              )}
-              <span>Ending in {orderData.source_data?.phone_number?.slice(-4)}</span>
-            </div>
-          </div>
-          
-          <h3 className="text-lg font-semibold mb-2">Delivery address (Home)</h3>
-          <p className="mb-1 text-sm ">{orderData.shipping_data?.first_name} {orderData.shipping_data?.email}</p>
-          <p className="mb-1">{orderData.shipping_data?.street}, {orderData.shipping_data?.city}, {orderData.shipping_data?.country}</p>
-          <p className="flex items-center">
-            {orderData.shipping_data?.phone_number}
-            <span className="ml-2 text-green-600 text-sm">Verified</span>
-          </p>
-        </div>
-        <div className="flex justify-end md:block">
-          <button className="bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded-xl"
-          onClick={async() => {
-            setloading(true)
-            const deletedID = await deleteOrderById(order.id)
-            if(deletedID){
-              toast.success('Order deleted successfully')
-              setloading(true)
-              router.refresh()
-            }
-            else{
-              toast.error('Something went wrong')
-            }
-          }}
-          >Delete Order</button>
-        </div>
-      </div>)
-    
+        );
       })}
     </div>
   );
@@ -149,9 +149,9 @@ export const Dashboard = ({ orders, loading, setloading }) => {
 
 function formatCreatedAt(timestamp) {
   if (!timestamp || !timestamp.seconds) return 'N/A';
-  
+
   const date = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000);
-  
+
   return date.toLocaleString('en-US', {
     year: 'numeric',
     month: 'short',
--- components/middleWare/MiddleWare.jsx
+++ components/middleWare/MiddleWare.jsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from "next/image";
-import logo from '@/public/logo.png'
+import logo from '@/public/logo.png';
 import { useState } from "react";
 import { Dashboard } from "../RealShit/Dashboard";
 import { getAllOrders } from "@/lib/utils";
@@ -13,94 +13,85 @@ export const MiddleWare = () => {
   const [orders, setorders] = useState([]);
   const [loading, setloading] = useState(false);
 
-
-const loginUser = async (email, password, e) => {
-  e.preventDefault();
-  setloading(true)
-    if (email ==process.env.NEXT_PUBLIC_USERNAME && password == process.env.NEXT_PUBLIC_PASSWORD) {
-        console.log('login success')
-        setloggedIN(true)
-        const orders=await getAllOrders();
-        console.log('orders', orders)
-        setorders(orders)
+  const loginUser = async (email, password, e) => {
+    e.preventDefault();
+    setloading(true);
+    if (email == process.env.NEXT_PUBLIC_USERNAME && password == process.env.NEXT_PUBLIC_PASSWORD) {
+      console.log('login success');
+      setloggedIN(true);
+      await refetchOrders();
     }
-  setloading(false)
+    setloading(false);
+  };
 
-}
+  const refetchOrders = async () => {
+    const orders = await getAllOrders();
+    setorders(orders);
+  };
 
   return (
-   
-   <div className="w-full">
-    {loggedIN && loading &&   (
-     <div className="fixed inset-0 flex items-center justify-center w-full h-full bg-black/50">
-     <LoadingDots/>
-     
-     </div>
-    )}
-    {loggedIN && orders.length==0 &&  !loading&& (
-     <div className="flex items-center justify-center w-full h-screen">
-     no active orders right now
-     
-     </div>
-    )}
-    {loggedIN ? <Dashboard orders={orders&&orders} loading={loading} setloading={setloading} getAllOrders={getAllOrders}/> : 
-    
-    
-    
-     <div className=" min-h-screen w-full flex justify-center flex-col items-center  gap-y-2">
-    <Image
-    src={logo}
-    width={200}
-    height={200}
-    alt="logo"
-    className="w-28"
-    />
-      <h1 className='text-3xl font-bold text-white'>Login</h1>
-      
-      <form onSubmit={(e) => loginUser(Username, Password, e)} className=" max-w-[500px] flex flex-col gap-y-4 w-full">
-       
-      <div className="w-full flex flex-col gap-y-2">
-        <label htmlFor="username" className="text-xl font-medium text-white">
-          E-mail
-        </label>
-        <input
-          id="username"
-          name="username"
-          type="email"
-          required
-          placeholder="Enter your E-mail"
-          onChange={(e) => setUsername(e.target.value)}
-          className="rounded-lg h-10 font-bold text-black text-md p-2"
-        />
-      </div>
-
-      <div className="w-full flex flex-col gap-y-2">
-        <label htmlFor="password" className="text-xl font-medium text-white">
-          Password
-        </label>
-        <div className="flex flex-col gap-y-4">
-          <input
-            id="password"
-            name="password"
-            type="password"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
-            className="rounded-lg h-10 font-bold text-black text-md p-2"
-          />
-          
+    <div className="w-full">
+      {loggedIN && loading && (
+        <div className="fixed inset-0 flex items-center justify-center w-full h-full bg-black/50">
+          <LoadingDots />
         </div>
-      </div>
-      
-        <button role='submit' className=' text-lg font-bold rounded-lg p-2  bg-blue-500 hover:bg-blue-600 duration-900'
-          
-          >
-          Login
-          </button>
-    </form>
-      
+      )}
+      {loggedIN && orders.length == 0 && !loading && (
+        <div className="flex items-center justify-center w-full h-screen">
+          no active orders right now
+        </div>
+      )}
+      {loggedIN ? (
+        <Dashboard orders={orders} refetchOrders={refetchOrders} />
+      ) : (
+        <div className="min-h-screen w-full flex justify-center flex-col items-center gap-y-2">
+          <Image
+            src={logo}
+            width={200}
+            height={200}
+            alt="logo"
+            className="w-28"
+          />
+          <h1 className='text-3xl font-bold text-white'>Login</h1>
+
+          <form onSubmit={(e) => loginUser(Username, Password, e)} className='mt-2'>
+            <div className="mb-4">
+              <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-transparent"
+                id="username"
+                type="text"
+                placeholder="Username"
+                value={Username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline bg-transparent"
+                id="password"
+                type="password"
+                placeholder="***************"
+                value={Password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex items-center justify-between w-full">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white w-full font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Log In
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
-}
-   </div>
   );
 };
